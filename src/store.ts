c171d3430bce28727589efcf1d7fcc61b81c35e4
Malejo01 <lizarragamauroalejandro@gmail.com>
2025-02-24 20:58:56 -0300

    se agregan los productos por boton al console log

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,16 @@
 import {create} from 'zustand'
 import { OrderItem } from './types'
+import { Product } from '@prisma/client'
 
 interface Store {
     order: OrderItem[]
+    addToOrder: (product: Product) => void
 }
 
 export const useStore = create<Store>(() => ({
-    order:[]
+    order:[],
+    addToOrder: (product) => {
+        console.log('Agregando', product)
+    }
+
 }))
