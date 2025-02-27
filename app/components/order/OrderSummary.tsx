7bd059a777446cb233794fc115cea30e8071dedf
Malejo01 <lizarragamauroalejandro@gmail.com>
2025-02-27 13:06:42 -0300

    agregado mensaje de error nombre toastify

--- app/components/order/OrderSummary.tsx
+++ app/components/order/OrderSummary.tsx
@@ -1,19 +1,40 @@
 'use client'
 
+import { useMemo } from "react"
 import { useStore } from "@/src/store"
 import ProductCard from "../products/ProductCard"
 import ProductDetails from "./ProductDetails"
-import { useMemo } from "react"
+import {toast} from 'react-toastify'
 import { formatCurrency } from "@/src/utils"
+import { createOrder } from "@/actions/create-order-action"
+import { OrderSchema } from "@/src/schema"
 
 export default function OrderSummary () {
     const order = useStore((state) => state.order) 
     const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+    
+    const handleCreateOrder = (formData: FormData) => {
+        const data = {
+            name: formData.get('name')
+        }
+
+        const result = OrderSchema.safeParse(data)
+       if (!result.success) {
+            result.error.issues.forEach((issue) => {
+                toast.error(issue.message)
+            })
+       }
+
+        return 
+
+        createOrder()
+    }
+
     return (
         <aside className="lg: h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
             <h1 className="text-4xl text-center font-black">Mi pedido</h1>
 
-            {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
+            {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio</p> : (
                 <div className="mt-5">
                     {order.map(item =>(
                         <ProductDetails
@@ -27,6 +48,24 @@ export default function OrderSummary () {
                             {formatCurrency(total)}
                         </span>
                     </p>
+                    <form 
+                        className="w-full mt-10 space-y-5"
+                        action={handleCreateOrder}
+                    >
+                        <input 
+                            type="text"
+                            placeholder='Tu Nombre'
+                            className="bg-white border border-gray-100 p-2 w-full"
+                            name="name"
+                        >
+                        
+                        </input>
+                        <input
+                            type="submit"
+                            className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+                            value='Confirmar Pedido'
+                        />
+                    </form>
                 </div>
             )}
         </aside>
---
+++ app/components/ui/ToastNotification.tsx
@@ -0,0 +1,16 @@
+"use client" 
+
+import "react-toastify/dist/ReactToastify.css"
+import {ToastContainer} from 'react-toastify'
+
+
+
+
+
+function ToastNotification() {
+        return (
+            <ToastContainer/>
+    )
+}
+
+export default ToastNotification
