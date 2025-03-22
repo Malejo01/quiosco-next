809a9cb1e014ac75aef3545eb8749b7232bee60c
Malejo01 <lizarragamauroalejandro@gmail.com>
2025-03-22 16:16:56 -0300

    funciona enviar y almacenar ordenes

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,15 @@
 import {z} from 'zod' 
 
 export const OrderSchema = z.object({
-    name: z.string().
-        min(1, 'Debes colocar tu nombre')
+    name: z.string()
+        .min(1, 'Debes colocar tu nombre'),
+    total:z.number()
+        .min(1,'Hay errores en la orden'),
+    order: z.array(z.object({
+        id: z.number(), 
+        name: z.string(),
+        price: z.number(),
+        quantity: z.number(),
+        subtotal: z.number()
+    }))
 })
