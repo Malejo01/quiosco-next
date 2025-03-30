2f9f1364a579f22af685c81b8e25c761e10d28a0
Malejo01 <lizarragamauroalejandro@gmail.com>
2025-03-30 19:34:18 -0300

    Funciona todoo

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -11,7 +11,7 @@ export default  function OrdersPage() {
     const url = '/admin/orders/api'
     const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
     const {data, error, isLoading} = useSWR<OrderWithProducts[]>(url, fetcher, {
-        refreshInterval:5000,
+        refreshInterval:10000,
         revalidateOnFocus: false
     })
 
--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,12 +1,39 @@
+"use client"
 import Logo from "../components/ui/Logo";
+import Heading from '../components/ui/Heading';
+import useSWR from 'swr';
+import { OrderWithProducts } from "@/src/types";
+import LatestOrderItem from "../components/order/LatestOrderItem";
 
 
 export default function OrdersPage() {
-    return (
+
+    const url = '/orders/api'
+    const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+    const {data, error, isLoading} = useSWR<OrderWithProducts[]>(url, fetcher, {
+        refreshInterval:5000,
+        revalidateOnFocus: false
+    })
+
+    if (isLoading) return (
+        <Heading>Cargando...</Heading>
+    )
+
+   if (data)  return (
         <div>
         <h1 className="text-center mt-20 text-6xl font-black"> Ordenes Listas</h1>
-        
+
         <Logo/>
+        {data.length ? (
+            <div className="grid grid-cols-2 gap-5 max-w-5xl mx-auto mt-10 ">
+                {data.map(order => (
+                    <LatestOrderItem
+                        key={order.id}
+                        order={order}
+                    />
+                ))}
+            </div>
+        ): <p className= 'text-center my-10'>No hay ordenes listas </p>}
         </div>
     )
 }
